1649c962e300314751298573ecc16c40713731c1
Barbara Washington <barbara.m.washington@gmail.com>
2018-06-02 20:03:03 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,17 @@
+require 'Nokogiri'
 require 'open-uri'
 require 'pry'
 
-class Scraper
+class Scraper #responsible for scraping the data from a webpage
 
   def self.scrape_index_page(index_url)
-    
+
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
 
+# Use metaprogramming to instantiate new instances of a class and add attributes to instances of a class.
